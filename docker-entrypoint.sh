c1e2e74cd61b619b5512647276489e9f5acd74e8
Matthew Medway <25482293+matthewmedway@users.noreply.github.com>
2019-03-10 13:08:46 -0400

    made restarts a thing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -ex
 
-pass=$(mkpasswd -m sha-512 -s $password) && \
-useradd -m -s /usr/bin/mysecureshell -p $pass $username -d /data
-mkdir -p /sshkeys/$username
-echo "$publickey" > /sshkeys/$username/authorized_keys
+#check if user already exists
+if id "$username" >/dev/null 2>&1; then
+    echo "User exists"
+else
+    echo "User does not exist"
+    pass=$(mkpasswd -m sha-512 -s "$password") && \
+    useradd -m -s /usr/bin/mysecureshell -p "$pass" "$username" -d /data
+    mkdir -p /sshkeys/"$username"
+    echo "$publickey" > /sshkeys/"$username"/authorized_keys
+fi
 
 exec "$@"
