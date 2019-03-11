7655db2722b704f5689f2c6d9f0926d244b947cd
Matthew Medway <25482293+matthewmedway@users.noreply.github.com>
2019-03-11 19:56:43 -0400

    clean logs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 #check if user already exists
 if id "$username" >/dev/null 2>&1; then
     #on docker restart
-    echo "User exists"
 else
     #on docker run
     pass=$(mkpasswd -m sha-512 -s "$password") && \
